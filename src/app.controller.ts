78532c9a034670cbae0599419f13585230d08604
jkpark104 <jkrang104@gmail.com>
2024-05-05 19:41:34 +0900

    feat: kakap-friend-list api 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,16 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('/profiles/me')
+  getMyProfile() {
+    return this.appService.getMyProfile();
+  }
+
+  @Get('/profiles')
+  getFriendProfiles() {
+    return this.appService.getFriendProfiles();
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { friendProfiles, myProfile } from 'src/mocks/data';
 
 @Injectable()
 export class AppService {
+  getMyProfile() {
+    return myProfile;
+  }
+
+  getFriendProfiles() {
+    return friendProfiles;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
