f02ff7ad138b469f84349685edcbbe6cf2434d33
jkpark104 <jkrang104@gmail.com>
2024-05-07 00:20:01 +0900

    ci: terraform test

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,13 +10,19 @@ terraform {
 provider "aws" {
   region = "ap-northeast-2"
 }
-resource "aws_instance" "kakao_friend_list_server" {
+
+variable "ec2_instance_name" {
+  description = "The name of the EC2 instance"
+  type        = string
+}
+
+resource "aws_instance" "ec2-instance" {
   ami                    = "ami-0e6f2b2fa0ca704d0"
   instance_type          = "t2.micro"
   key_name               = "terraform_key_pair"
   vpc_security_group_ids = ["sg-01c189184d271e943"]
 
   tags = {
-    Name = "kakao-friend-list-server"
+    Name = var.ec2_instance_name
   }
 }
