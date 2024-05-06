5a464eacfa0dfbb32a1b811510651cafaad459a5
jkpark104 <jkrang104@gmail.com>
2024-05-06 11:03:22 +0900

    ci: terraform test4

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "kakao_friend_list_backend" {
   ami                    = "ami-0e6f2b2fa0ca704d0"
   instance_type          = "t2.micro"
   key_name               = "terraform_key_pair"
-  vpc_security_group_ids = ["vpc-0cd1ac93f8441226f"]
+  vpc_security_group_ids = ["sg-01c189184d271e943"]
 
   tags = {
     Name = "kakao_friend_list_backend"
