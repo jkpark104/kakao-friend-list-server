a96328379d3ecac364537504f5bd9bc8ef6629d4
jkpark104 <jkrang104@gmail.com>
2024-05-06 09:49:11 +0900

    ci: terraform test1

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "kakao_friend_list_backend" {
+  ami           = "ami-0e6f2b2fa0ca704d0"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.terraform_key_pair.key_name
+  tags = {
+    Name = "kakao_friend_list_backend"
+  }
+}
---
+++ terraform/private_key.tf
@@ -0,0 +1,11 @@
+
+resource "tls_private_key" "terraform_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "terraform_key_pair" {
+  key_name   = "terraform_key_pair"
+  public_key = tls_private_key.terraform_private_key.public_key_openssh
+}
+
