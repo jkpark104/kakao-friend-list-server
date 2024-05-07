38c30e5c08ccc2580f919ce0624b40d6308cb97a
jkpark104 <jkrang104@gmail.com>
2024-05-07 21:18:21 +0900

    ci: test

--- terraform/security_group/main.tf
+++ terraform/security_group/main.tf
@@ -20,38 +20,52 @@ resource "aws_security_group" "security_group" {
   name        = var.security_group_name
   description = "Allow HTTP and SSH inbound traffic"
   vpc_id      = "vpc-0cd1ac93f8441226f"
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-  }
-
-  ingress {
-    description = "Custom TCP"
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-  }
 }
+
+resource "aws_security_group_rule" "sg_ingress_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0/0"]
+  security_group_id = aws_security_group.security_group.id
+}
+
+
+resource "aws_security_group_rule" "sg_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0/0"]
+  security_group_id = aws_security_group.security_group.id
+}
+
+resource "aws_security_group_rule" "sg_ingress_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0/0"]
+  security_group_id = aws_security_group.security_group.id
+}
+
+resource "aws_security_group_rule" "sg_ingress_custom_tcp" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0/0"]
+  security_group_id = aws_security_group.security_group.id
+}
+
+resource "aws_security_group_rule" "sg_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0/0"]
+  security_group_id = aws_security_group.security_group.id
+}
+
+
