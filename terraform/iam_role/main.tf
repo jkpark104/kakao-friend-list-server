49cc98c6a8b145be49802fe182a6e917672168ed
jkpark104 <jkrang104@gmail.com>
2024-05-09 21:15:01 +0900

    ci: test

---
+++ terraform/iam_role/main.tf
@@ -0,0 +1,50 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+variable "role_name" {
+  description = "The name of the IAM role"
+  type        = string
+}
+
+resource "aws_iam_role" "ec2_code_deploy_role" {
+  name = var.role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_code_deploy_profile" {
+  name = "${var.role_name}-profile"
+  role = aws_iam_role.ec2_code_deploy_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "code_deploy_policy" {
+  role       = aws_iam_role.ec2_code_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ec2_code_deploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
