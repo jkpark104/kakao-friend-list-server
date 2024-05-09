a94921a24f75208acbf99e9a65a2b9485bf25357
jkpark104 <jkrang104@gmail.com>
2024-05-09 20:49:41 +0900

    ci: test

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -34,28 +34,24 @@ resource "aws_instance" "ec2_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-              apt-get update
-              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              sudo apt-get update
+              sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-              apt-get update
-              apt-get install -y docker-ce docker-ce-cli containerd.io
-              systemctl start docker
-              systemctl enable docker
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo apt-get update
+              sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-              # Docker Compose 설치
-              curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              chmod +x /usr/local/bin/docker-compose
+              sudo curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+              sudo chmod +x /usr/local/bin/docker-compose
 
-              # Docker Compose 환경 변수 설정
-              echo "export DOCKER_COMPOSE_VERSION=1.29.2" >> /etc/profile
-
-              # AWS CodeDeploy Agent 설치
-              apt-get install -y ruby wget
+              sudo apt update              
+              sudo apt install ruby-full -y
+              sudo apt install wget
               cd /home/ubuntu
-              wget https://aws-codedeploy-ap-northeast-2.s3.amazonaws.com/latest/install
+              wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
               chmod +x ./install
-              ./install auto
+              sudo ./install auto
+              sudo service codedeploy-agent start
               EOF
 
   tags = {
