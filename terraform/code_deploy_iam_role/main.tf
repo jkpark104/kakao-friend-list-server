35032d7320040405e09eb6576a0297b1eb67c3ec
jkpark104 <jkrang104@gmail.com>
2024-05-09 23:35:24 +0900

    ci: test

--- terraform/code_deploy_iam_role/main.tf
+++ terraform/code_deploy_iam_role/main.tf
@@ -27,7 +27,7 @@ resource "aws_iam_role" "code_deploy_role" {
         Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
-          Service = "ec2.amazonaws.com"
+          Service = "codedeploy.amazonaws.com"
         }
       },
     ]
